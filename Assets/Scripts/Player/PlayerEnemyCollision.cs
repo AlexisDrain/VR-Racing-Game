8d3eec3497e1850a010b37d02878937c7af05be0
Alexis <alexisdrain97@gmail.com>
2023-03-09 13:59:59 -0800

    description, sources, whoosh sfx

--- Assets/Scripts/Player/PlayerEnemyCollision.cs
+++ Assets/Scripts/Player/PlayerEnemyCollision.cs
@@ -10,6 +10,7 @@ public class PlayerEnemyCollision : MonoBehaviour
 {
     public bool killPlayer;
     public bool playWhooshSFX;
+    public Vector2 sfxPitchRange = new Vector2(0.6f, 1.5f);
 
     // Update is called once per frame
     void OnTriggerEnter(Collider col)
@@ -21,6 +22,7 @@ public class PlayerEnemyCollision : MonoBehaviour
                 Destroy(col.gameObject);
             }
             if (playWhooshSFX) {
+                GetComponent<AudioSource>().pitch = Random.Range(sfxPitchRange.x, sfxPitchRange.y);
                 GetComponent<AudioSource>().Play();
             }
         }

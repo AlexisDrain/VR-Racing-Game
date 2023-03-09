908a095f6e79dfca84b21fb7c15bd15ce764e8b6
Alexis <alexisdrain97@gmail.com>
2023-03-08 17:25:26 -0800

    Added TEST music - Adjusted difficulty

--- Assets/Scripts/GenerateObstacles.cs
+++ Assets/Scripts/GenerateObstacles.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GenerateObstacles : MonoBehaviour
 {
+	public List<Vector2> difficultyTimeGenerateDelay = new List<Vector2>();
+	public int currentDifficulty;
+	public int surpassedObstacles = 0;
+
 	public Vector2 timeGenerateDelayRange = new Vector2(1f, 3f);
 	public List<GameObject> obstaclePrefabs;
 
@@ -30,11 +34,19 @@ public class GenerateObstacles : MonoBehaviour
 		prefebOj.transform.parent = enemyCollectionTrans;
 		prefebOj.transform.position = new Vector3(0f, 0f, GameManager.playerXRig.transform.position.z + createDistance);
 
+		surpassedObstacles += 1;
+
+		if(surpassedObstacles > 5 && currentDifficulty == 0) {
+			currentDifficulty = 1;
+		}
 	}
 	public void FixedUpdate() {
 		timeUntilGenerate -= Time.deltaTime;
 		if (timeUntilGenerate <= 0f) {
-			timeUntilGenerate = Random.Range(timeGenerateDelayRange.x, timeGenerateDelayRange.y);
+
+			Vector2 delayRange = difficultyTimeGenerateDelay[currentDifficulty];
+			timeUntilGenerate = Random.Range(delayRange.x, delayRange.y);
+
 			Generate();
 		}
 	}

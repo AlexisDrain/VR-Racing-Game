77a1b21626f951ec14d16437c25dcfaeec3ed9ed
Alexis <alexisdrain97@gmail.com>
2023-03-23 14:47:59 -0700

    new obstacle (astrisk). death

--- Assets/Scripts/Player/PlayerEnemyCollision.cs
+++ Assets/Scripts/Player/PlayerEnemyCollision.cs
@@ -8,35 +8,54 @@ using UnityEngine;
 
 public class PlayerEnemyCollision : MonoBehaviour
 {
-    public bool killPlayer;
-    public bool playWhooshSFX;
-    public Vector2 sfxPitchRange = new Vector2(0.6f, 1.5f);
+	public bool killPlayer;
+	public bool playWhooshSFX;
+	public Vector2 sfxPitchRange = new Vector2(0.6f, 1.5f);
 
-    private bool canWooshAgain = true;
+	private bool canWooshAgain = true;
 
-    // Update is called once per frame
-    void OnTriggerEnter(Collider col)
-    {
-        if(col.CompareTag("Enemy")) {
-            if (killPlayer && GameManager.playerIsAlive) {
-                print("Collision with: " + col.gameObject.name);
-                GameManager.EndGame();
-                Destroy(col.gameObject);
-            }
-            if (playWhooshSFX && canWooshAgain && GameManager.playerIsAlive) {
+	/*
+	void OnCollisionEnter(Collision col) {
+		print(col);
+		if (col.collider.CompareTag("Enemy")) {
+			if (playWhooshSFX && canWooshAgain && GameManager.playerIsAlive) {
 				canWooshAgain = false;
-                StartCoroutine(WhooshDelay());
+				StartCoroutine(WhooshDelay());
 
 				GetComponent<AudioSource>().pitch = Random.Range(sfxPitchRange.x, sfxPitchRange.y);
-                GetComponent<AudioSource>().Play();
-            }
-        }
-    }
+				GetComponent<AudioSource>().Play();
+			}
+			if (killPlayer && GameManager.playerIsAlive) {
+				print("Collision with: " + col.gameObject.name);
+				GameManager.EndGame();
+				//Destroy(col.gameObject);
+			}
+		}
 
-    private IEnumerator WhooshDelay() {
+	}
+	*/
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.CompareTag("Enemy")) {
+			if (playWhooshSFX && canWooshAgain && GameManager.playerIsAlive) {
+				canWooshAgain = false;
+				StartCoroutine(WhooshDelay());
 
-        yield return new WaitForSeconds(0.1f);
-        canWooshAgain = true;
+				GetComponent<AudioSource>().pitch = Random.Range(sfxPitchRange.x, sfxPitchRange.y);
+				GetComponent<AudioSource>().Play();
+			}
+			if (killPlayer && GameManager.playerIsAlive) {
+				print("Collision with: " + col.gameObject.name);
+				GameManager.EndGame();
+				//Destroy(col.gameObject);
+			}
+		}
+	}
+
+	private IEnumerator WhooshDelay() {
+
+		yield return new WaitForSeconds(0.1f);
+		canWooshAgain = true;
 
 	}
 }

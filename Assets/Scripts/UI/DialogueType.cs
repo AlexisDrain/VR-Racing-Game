e45e2f54bcb34f2936ad83b517705e8257390e09
Alexis <alexisdrain97@gmail.com>
2023-05-22 14:40:29 -0700

    intro story

---
+++ Assets/Scripts/UI/DialogueType.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/*
+* Author: Alexis Clay Drain
+*/
+public class DialogueType : MonoBehaviour
+{
+
+    [Header("stuff to delete at the end")]
+    public Text text1;
+    public Text text2;
+
+    [Header("audio")]
+    public AudioSource textAudioSource;
+
+    private string finalText = "";
+    private Text myText;
+
+    private void Start() {
+        myText = GetComponent<Text>();
+
+        finalText = GetComponent<Text>().text;
+        myText.text = "";
+
+        StartCoroutine(Typewriter());
+    }
+
+    public void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            StopCoroutine(Typewriter());
+            StartCoroutine(startupGame());
+        }
+    }
+
+    IEnumerator startupGame() {
+        text1.enabled = false;
+        text2.enabled = false;
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Chasm_WebGL_Scene");
+    }
+
+    IEnumerator Typewriter() {
+        yield return new WaitForSeconds(2f);
+        GetComponent<AudioSource>().Play();
+        foreach (char c in finalText) {
+            if (c != ' ' || c != '\n') {
+                textAudioSource.Play();
+            }
+            myText.text += c;
+            yield return new WaitForSeconds(.01f);
+        }
+        yield return new WaitForSeconds(5f);
+        StartCoroutine(startupGame());
+        yield return null;
+    }
+
+}
